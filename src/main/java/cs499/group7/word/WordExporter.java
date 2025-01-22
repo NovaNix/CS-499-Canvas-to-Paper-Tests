e7ea1f8948c528b65d8567995f581eaa35905f94
Michael Nix <michael.g.nix@gmail.com>
2025-01-22 15:39:05 -0600

    Added word skeleton code

--- src/main/java/cs499/group7/word/WordExporter.java
+++ src/main/java/cs499/group7/word/WordExporter.java
@@ -3,4 +3,21 @@ package cs499.group7.word;
 public class WordExporter 
 {
 
+	// TODO add template files, add function parameters
+	
+	public WordExporter()
+	{
+		
+	}
+	
+	public void exportTest()
+	{
+		
+	}
+	
+	public void exportAnswerKey()
+	{
+		
+	}
+	
 }
