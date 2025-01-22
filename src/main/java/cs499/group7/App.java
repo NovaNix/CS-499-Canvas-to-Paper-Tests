466d47cd209e84fd7ffbc362bc406c9f19ec1d86
Michael Nix <michael.g.nix@gmail.com>
2025-01-22 15:40:41 -0600

    Added starter code

--- src/main/java/cs499/group7/App.java
+++ src/main/java/cs499/group7/App.java
@@ -2,14 +2,28 @@ package cs499.group7;
 
 import javax.swing.SwingUtilities;
 
+import cs499.group7.data.Project;
+import cs499.group7.ui.UIManager;
+
 public class App 
 {
 
+	private static UIManager ui;
+	
+	private static Project project;
+	
 	public static void main(String[] args)
 	{
+		// Create a new blank project
+		project = new Project();
+		
+		// Initiate the UI code
 		SwingUtilities.invokeLater(() -> {
 			// The UI needs to be started on the event threads
+			ui = new UIManager();
 		});
 	}
+
+	// TODO add event listeners
 	
 }
