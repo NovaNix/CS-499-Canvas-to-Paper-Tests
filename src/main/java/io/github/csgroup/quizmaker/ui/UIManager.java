a356fb911837a8ce2798d04d232dc7c415869c86
emilykpalmer <e.palmermay2001@gmail.com>
2025-01-28 15:09:13 -0600

    Adding the frame and JMenu for the UI

--- src/main/java/io/github/csgroup/quizmaker/ui/UIManager.java
+++ src/main/java/io/github/csgroup/quizmaker/ui/UIManager.java
@@ -2,11 +2,15 @@ package io.github.csgroup.quizmaker.ui;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * The central manager for the user interface. 
  * 
- * @author 
+ * @author Emily Palmer
  */
 public class UIManager 
 {
@@ -16,8 +20,45 @@ public class UIManager
 	public UIManager()
 	{
 		logger.info("Starting UI");
-		
-		// YOUR CODE HERE
 	}
-	
+        
+        // add (probably javadoc comment) about what this method does      
+        public void createHomeFrame() 
+        {
+            // main JFrame that will host all JPanels for the system
+            JFrame homeFrame = new JFrame("Canvas to Paper Tests");
+               
+            //size of the JFrame (width, height)
+            homeFrame.setSize(685, 495);
+            
+            // will contain the menus "File" and "About"
+            JMenuBar menuBar = new JMenuBar();
+            // "File" menu
+            JMenu fileMenu = new JMenu("File");
+            // items for the user to select in the "File" menu
+            JMenuItem importFileItem = new JMenuItem("Import QTI Files");
+            JMenuItem exportFileItem = new JMenuItem("Export QTI Files");
+            JMenuItem exportDocumentItem = new JMenuItem("Export Word Document");            
+            // add items to the JMenu fileMenu
+            fileMenu.add(importFileItem);
+            fileMenu.add(exportFileItem);
+            fileMenu.add(exportDocumentItem);       
+            // add JMenu "File" to the JMenuBar menuBar
+            menuBar.add(fileMenu);
+            
+            // "About" menu
+            JMenu aboutMenu = new JMenu("About");               
+            // add JMenu "About" to the JMenuBar menuBar
+            menuBar.add(aboutMenu);
+        
+            // add the JMenuBar menuBar to homeFrame
+            homeFrame.setJMenuBar(menuBar);
+                                                
+            // if the JFrame is closed terminate the running program
+            homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            // makes the JFrame appear in the center of the screen
+            homeFrame.setLocationRelativeTo(null);
+            // make the JFrame visible
+            homeFrame.setVisible(true);  
+        }	
 }
