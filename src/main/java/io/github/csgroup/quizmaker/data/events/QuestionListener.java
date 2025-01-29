185e659fec49d7abff2652be51b48415041de208
Michael Nix <michael.g.nix@gmail.com>
2025-01-29 11:50:56 -0600

    Added some documentation to QuestionListener

--- src/main/java/io/github/csgroup/quizmaker/data/events/QuestionListener.java
+++ src/main/java/io/github/csgroup/quizmaker/data/events/QuestionListener.java
@@ -3,6 +3,11 @@ package io.github.csgroup.quizmaker.data.events;
 import io.github.csgroup.quizmaker.data.Question;
 import io.github.csgroup.quizmaker.data.events.question.QuestionEvent;
 
+/**
+ * An object that runs code when {@link QuestionEvent QuestionEvents} are fired
+ * 
+ * @author Michael Nix
+ */
 public interface QuestionListener 
 {
 
