16b4626872bdf09d9ad6d10f873f238c84b1127a
Michael Nix <michael.g.nix@gmail.com>
2025-02-24 13:40:12 -0600

    Added FillInTheBlank Question

---
+++ src/main/java/io/github/csgroup/quizmaker/data/answers/BlankAnswer.java
@@ -0,0 +1,14 @@
+package io.github.csgroup.quizmaker.data.answers;
+
+import java.util.List;
+
+import io.github.csgroup.quizmaker.data.Answer;
+import io.github.csgroup.quizmaker.data.Label;
+
+public class BlankAnswer 
+{
+	String tag;
+	
+	Answer correctAnswer;
+	List<Label> possibleAnswers;
+}
---
+++ src/main/java/io/github/csgroup/quizmaker/data/questions/FillInTheBlankQuestion.java
@@ -0,0 +1,21 @@
+package io.github.csgroup.quizmaker.data.questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.github.csgroup.quizmaker.data.answers.BlankAnswer;
+
+public class FillInTheBlankQuestion 
+{
+
+	private Map<String, BlankAnswer> answers = new HashMap<String, BlankAnswer>();
+	
+	/**
+	 * Whether the possible answers for each blank will be shown underneath the question
+	 */
+	private boolean showAnswers;
+	
+	// getTags, returns all of the tags found in the question label
+	// getAnswer(tag)
+	
+}
