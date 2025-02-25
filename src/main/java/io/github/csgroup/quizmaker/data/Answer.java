a8d5244457c06eb46923ebc82b495f791ced39be
Michael Nix <michael.g.nix@gmail.com>
2025-02-25 14:30:52 -0600

    Added answer events

--- src/main/java/io/github/csgroup/quizmaker/data/Answer.java
+++ src/main/java/io/github/csgroup/quizmaker/data/Answer.java
@@ -1,5 +1,11 @@
 package io.github.csgroup.quizmaker.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.csgroup.quizmaker.data.events.AnswerListener;
+import io.github.csgroup.quizmaker.data.events.answer.AnswerEvent;
+
 /**
  * An answer to a {@link Question Question}
  * 
@@ -27,5 +33,43 @@ public abstract class Answer
 	public String toString()
 	{
 		return asText();
+	}
+	
+	// Event Processing
+	
+	private static List<AnswerListener> globalListeners = new ArrayList<AnswerListener>();
+	private List<AnswerListener> localListeners = new ArrayList<AnswerListener>();
+
+	public void addListener(AnswerListener listener)
+	{
+		localListeners.add(listener);
+	}
+
+	public void removeListener(AnswerListener listener)
+	{
+		localListeners.remove(listener);
+	}
+
+	public static void addGlobalListener(AnswerListener listener)
+	{
+		globalListeners.add(listener);
+	}
+
+	public static void removeGlobalListener(AnswerListener listener)
+	{
+		globalListeners.remove(listener);
+	}
+
+	protected void fireEvent(AnswerEvent event)
+	{
+		for (var listener : globalListeners)
+		{
+			listener.onAnswerEvent(this, event);
+		}
+
+		for (var listener : localListeners)
+		{
+			listener.onAnswerEvent(this, event);
+		}
 	}
 }
