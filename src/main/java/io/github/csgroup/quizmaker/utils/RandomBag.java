f065d7c01818994fd975f1edd26af31a593f64ef
Michael Nix <michael.g.nix@gmail.com>
2025-03-06 18:52:16 -0600

    Implemented RandomBag

--- src/main/java/io/github/csgroup/quizmaker/utils/RandomBag.java
+++ src/main/java/io/github/csgroup/quizmaker/utils/RandomBag.java
@@ -1,5 +1,40 @@
 package io.github.csgroup.quizmaker.utils;
 
-public class RandomBag {
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
 
+public class RandomBag<T> 
+{
+	
+	private Queue<T> bag;
+	
+	public RandomBag(List<T> contents)
+	{
+		this(contents, new Random());
+	}
+	
+	public RandomBag(List<T> contents, Random random)
+	{
+		List<T> shuffled = new ArrayList<T>(contents); // We need a copy of the list to avoid shuffling the original
+		Collections.shuffle(shuffled, random);
+		
+		bag = new ArrayDeque<T>(shuffled);
+	}
+	
+	public T next()
+	{
+		return bag.poll();
+	}
+	
+	public T peek()
+	{
+		return bag.peek();
+	}
+	
+	
+	
 }
