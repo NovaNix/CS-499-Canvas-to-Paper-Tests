533af01b74769f42b4a786ad1561304e3c576ceb
Michael Nix <michael.g.nix@gmail.com>
2025-04-17 04:10:50 -0500

    Created generated tab

---
+++ src/main/java/io/github/csgroup/quizmaker/ui/GeneratedTab.java
@@ -0,0 +1,19 @@
+package io.github.csgroup.quizmaker.ui;
+
+import javax.swing.JComponent;
+
+import io.github.csgroup.quizmaker.data.Project;
+
+public class GeneratedTab extends JComponent
+{
+
+	private Project project;
+	
+	public GeneratedTab(Project p)
+	{
+		this.project = p;
+	}
+	
+	
+	
+}
