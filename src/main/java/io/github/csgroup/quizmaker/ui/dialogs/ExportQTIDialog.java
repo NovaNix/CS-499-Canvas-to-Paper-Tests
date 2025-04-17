6a398d4e0f9c4939cabfba222df276b2dac6dfd9
emilykpalmer <e.palmermay2001@gmail.com>
2025-04-17 10:46:28 -0500

    changes

--- src/main/java/io/github/csgroup/quizmaker/ui/dialogs/ExportQTIDialog.java
+++ src/main/java/io/github/csgroup/quizmaker/ui/dialogs/ExportQTIDialog.java
@@ -174,11 +174,15 @@ public class ExportQTIDialog
             String exportFilePath = getPath();
             if (exportFilePath != null)
             {
-                Path path = Paths.get(exportFilePath);
-                QTIWriter writer = new QTIWriter();
-                writer.writeProject(project, path);                             
-                // close the frame
-                exportQTIFrame.dispose();
+                try
+                {
+                    Path path = Paths.get(exportFilePath);
+                    QTIWriter writer = new QTIWriter();
+                    writer.writeProject(project, path);                             
+                    // close the frame
+                    exportQTIFrame.dispose();
+                }
+                catch (IOException n) {}
             }
         });
         
